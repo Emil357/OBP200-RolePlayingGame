57f5f973b014d959242683cfc7ac199a04b8ad45
Emil357 <emil.bergstrom@student.hv.se>
2026-03-12 11:48:56 +0100

    Har laggt in olia variabler för klassen ChosenCharacter

---
+++ OBP200-RolePlayingGame/ChosenCharacter.cs
@@ -0,0 +1,13 @@
+﻿namespace OBP200_RolePlayingGame;
+
+public class ChosenCharacter {
+    public string Name { get; set; }
+    public string TypeOfCharacter { get; set; }
+    public int MaxHealth { get; set; }
+    public int CurrentHealth  { get; set; }
+    public int AttackDamage { get; set; }
+    public int Defense { get; set; }
+    public int Potions { get; set; }
+    public int Gold { get; set; }
+    
+}
