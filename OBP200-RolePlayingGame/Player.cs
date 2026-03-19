3b9f5f9f855551acb84526bae4dbbaf57db2170b
Emil357 <emil.bergstrom@student.hv.se>
2026-03-19 16:13:52 +0100

    Jag gjorde en konstruktor för Player och även en lista som föremål kan läggas i. Satte även start värden på Xp, Gold och CurrentHealth

---
+++ OBP200-RolePlayingGame/Player.cs
@@ -0,0 +1,31 @@
+﻿namespace OBP200_RolePlayingGame;
+
+public class Player {
+    public string Name { get; set; }
+    public string TypeOfCharacter { get; set; }
+    public int MaxHealth { get; set; }
+    public int CurrentHealth  { get; set; }
+    public int AttackDamage { get; set; }
+    public int Defense { get; set; }
+    public int Potions { get; set; }
+    public int Gold { get; set; }
+    public int Xp { get; set; }
+    public int Level { get; set; }
+    
+    
+    public List<string> Inventory { get; set; }
+
+    public Player(string name, string typeOfCharacter, int maxHealth, int attackDamage, int defense, int potions, int gold, List<string> inventory) {
+        Name = name;
+        TypeOfCharacter = typeOfCharacter;
+        MaxHealth = maxHealth;
+        CurrentHealth = maxHealth;
+        AttackDamage = attackDamage;
+        Defense = defense;
+        Potions = potions;
+        Gold = gold;
+        Xp = 0;
+        Level = 1;
+        Inventory = inventory;
+    }
+}
