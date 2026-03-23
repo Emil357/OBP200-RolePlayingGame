5ef70babe214e63cc1ff7627492a7985e2bc7ca4
Emil357 <emil.bergstrom@student.hv.se>
2026-03-23 14:44:19 +0100

    Skapa character klass (#3)
    * Skapandet av character klass
    
    Jag skapade en abstract klass för character för att player och enemy har många gemensama variabler. Och jag gjorde den abstract för att skydda så att man måste skapa en Player eller Enemy så att man inte skapar en generisk character.
    
    * Änadrade i klassen Player och Enemy
    
    klasserna får ärva från character klassen.
    Jag tog bort de variabler som finns i character klassen och behöll det som klassen hade personligt.
    
    * Fixade stav fel i character klassen
    
    Hade stavat fel på några ord i klassen som gjorde att program klassen inte gick och köra men men har återgärdat det.
    
    * Tog bort död kod
    
    Tog bort kod som användas som ett säkerhetsnät ifall saker i koden inte funkar men nu har jag byt ut så att dessa fel inte kan uppstå med hjälp av mina klasser.

---
+++ OBP200-RolePlayingGame/Character.cs
@@ -0,0 +1,17 @@
+﻿namespace OBP200_RolePlayingGame;
+
+public abstract class Character {
+    public string Name { get; set; }
+    public int MaxHealth { get; set; }
+    public int CurrentHealth { get; set; }
+    public int AttackDamage { get; set; }
+    public int Defense { get; set; }
+    
+    public Character(string name, int maxHealth, int attackDamage,  int defense) {
+        Name = name;
+        MaxHealth = maxHealth;
+        CurrentHealth = maxHealth;
+        AttackDamage = attackDamage;
+        Defense = defense;
+    }
+}
--- OBP200-RolePlayingGame/Player.cs
+++ OBP200-RolePlayingGame/Player.cs
@@ -1,12 +1,7 @@
 ﻿namespace OBP200_RolePlayingGame;
 
-public class Player {
-    public string Name { get; set; }
+public class Player : Character{
     public string TypeOfCharacter { get; set; }
-    public int MaxHealth { get; set; }
-    public int CurrentHealth  { get; set; }
-    public int AttackDamage { get; set; }
-    public int Defense { get; set; }
     public int Potions { get; set; }
     public int Gold { get; set; }
     public int Xp { get; set; }
@@ -15,13 +10,8 @@ public class Player {
     
     public List<string> Inventory { get; set; }
 
-    public Player(string name, string typeOfCharacter, int maxHealth, int attackDamage, int defense, int potions, int gold, List<string> startingInventory) {
-        Name = name;
+    public Player(string name, string typeOfCharacter, int maxHealth, int attackDamage, int defense, int potions, int gold, List<string> startingInventory) : base(name, maxHealth, attackDamage, defense) {
         TypeOfCharacter = typeOfCharacter;
-        MaxHealth = maxHealth;
-        CurrentHealth = maxHealth;
-        AttackDamage = attackDamage;
-        Defense = defense;
         Potions = potions;
         Gold = gold;
         Xp = 0;
