7dc4db6b7f28eeb5bd6d7a07466b83b449bfac21
Emil357 <emil.bergstrom@student.hv.se>
2026-03-23 14:25:07 +0100

    Änadrade i klassen Player och Enemy
    klasserna får ärva från character klassen.
    Jag tog bort de variabler som finns i character klassen och behöll det som klassen hade personligt.

--- OBP200-RolePlayingGame/Enemy.cs
+++ OBP200-RolePlayingGame/Enemy.cs
@@ -1,22 +1,12 @@
 ﻿namespace OBP200_RolePlayingGame;
 
-public class Enemy {
+public class Enemy : Character{
     public string Type { get; set; }
-    public string Name { get; set; }
-    public int MaxHealth { get; set; }
-    public int CurrentHealth { get; set; }
-    public int AttackDamage { get; set; }
-    public int Defense { get; set; }
     public int XPReward { get; set; }
     public int GoldReward { get; set; }
 
-    public Enemy(string type, string name, int maxHealth, int attackDamage, int defense, int xpReward, int goldReward) {
+    public Enemy(string type, string name, int maxHealth, int attackDamage, int defense, int xpReward, int goldReward) : base(name, maxHealth, attackDamage, defense) {
         Type = type;
-        Name = name;
-        MaxHealth = maxHealth;
-        CurrentHealth = maxHealth;
-        AttackDamage = attackDamage;
-        Defense = defense;
         XPReward = xpReward;
         GoldReward = goldReward;
     }
