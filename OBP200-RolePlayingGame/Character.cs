0a48ff9cf9563db6799a5a8ffdb4ed437e2cda78
Emil357 <emil.bergstrom@student.hv.se>
2026-03-23 14:07:02 +0100

    Skapandet av character klass
    Jag skapade en abstract klass för character för att player och enemy har många gemensama variabler. Och jag gjorde den abstract för att skydda så att man måste skapa en Player eller Enemy så att man inte skapar en generisk character.

---
+++ OBP200-RolePlayingGame/Character.cs
@@ -0,0 +1,17 @@
+﻿namespace OBP200_RolePlayingGame;
+
+public abstract class Character {
+    public string Name { get; set; }
+    public int MaxHealt { get; set; }
+    public int CurrentHealt { get; set; }
+    public int AttackDamage { get; set; }
+    public int Defense { get; set; }
+    
+    public Character(string name, int maxHealt, int attackDamage,  int defense) {
+        Name = name;
+        MaxHealt = maxHealt;
+        CurrentHealt = maxHealt;
+        AttackDamage = attackDamage;
+        Defense = defense;
+    }
+}
