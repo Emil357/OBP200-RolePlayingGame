6c2daedb154cec2431c71d4a8074e0a2cef42a15
Emil357 <emil.bergstrom@student.hv.se>
2026-03-27 11:56:45 +0100

    Namnändring av variabel
    Ändrade XPReward till XpReward för att följa samma struktur som jag har för de andra variabelnamnen.

--- OBP200-RolePlayingGame/Enemy.cs
+++ OBP200-RolePlayingGame/Enemy.cs
@@ -2,12 +2,12 @@
 
 public class Enemy : Character{
     public string Type { get; set; }
-    public int XPReward { get; set; }
+    public int XpReward { get; set; }
     public int GoldReward { get; set; }
 
     public Enemy(string type, string name, int maxHealth, int attackDamage, int defense, int xpReward, int goldReward) : base(name, maxHealth, attackDamage, defense) {
         Type = type;
-        XPReward = xpReward;
+        XpReward = xpReward;
         GoldReward = goldReward;
     }
 }
